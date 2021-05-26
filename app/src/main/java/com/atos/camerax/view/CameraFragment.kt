df8691895e3c15ee527440a23c1ab0e372da9bdc
Sandip <snsan1109@gmail.com>
2021-05-26 20:03:16 +0530

    Initial commit

---
+++ app/src/main/java/com/atos/camerax/view/CameraFragment.kt
@@ -0,0 +1,177 @@
+package com.atos.camerax.view
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import android.os.Bundle
+import android.os.Environment
+import android.util.DisplayMetrics
+import android.util.Log
+import android.util.Rational
+import android.util.Size
+import android.view.*
+import android.widget.Toast
+import androidx.camera.core.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.atos.camerax.R
+import kotlinx.android.synthetic.main.fragment_camera.*
+import java.io.BufferedInputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.net.URL
+import java.net.URLConnection
+import java.util.*
+
+
+class CameraFragment : Fragment() {
+
+    private var lensFacing = CameraX.LensFacing.BACK
+    private val TAG = "CameraFragment"
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_camera, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (allPermissionsGranted()) {
+            texture.post { startCamera() }
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(), REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+
+        // Every time the provided texture view changes, recompute layout
+        texture.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
+            updateTransform()
+        }
+
+    }
+
+
+    private fun startCamera() {
+        val metrics = DisplayMetrics().also { texture.display.getRealMetrics(it) }
+        val screenSize = Size(metrics.widthPixels, metrics.heightPixels)
+        val screenAspectRatio = Rational(metrics.widthPixels, metrics.heightPixels)
+
+        val previewConfig = PreviewConfig.Builder().apply {
+            setLensFacing(lensFacing)
+            setTargetResolution(screenSize)
+            setTargetAspectRatio(screenAspectRatio)
+            setTargetRotation(requireActivity().getWindowManager().getDefaultDisplay().rotation)
+            setTargetRotation(texture.display.rotation)
+        }.build()
+
+        val preview = Preview(previewConfig)
+        preview.setOnPreviewOutputUpdateListener {
+            texture.setSurfaceTexture(it.surfaceTexture)
+            updateTransform()
+        }
+
+
+        // Create configuration object for the image capture use case
+        val imageCaptureConfig = ImageCaptureConfig.Builder()
+            .apply {
+                setLensFacing(lensFacing)
+                setTargetAspectRatio(screenAspectRatio)
+                setTargetRotation(texture.display.rotation)
+                setCaptureMode(ImageCapture.CaptureMode.MAX_QUALITY)
+            }.build()
+
+        // Build the image capture use case and attach button click listener
+        val imageCapture = ImageCapture(imageCaptureConfig)
+        val root: String = Environment.getExternalStorageDirectory().toString();
+        btn_take_picture.setOnClickListener {
+
+            val file = File("$root/saved_images")
+
+            imageCapture.takePicture(file,
+                object : ImageCapture.OnImageSavedListener {
+                    override fun onError(
+                        error: ImageCapture.UseCaseError,
+                        message: String, exc: Throwable?
+                    ) {
+                        val msg = "Photo capture failed: $message"
+                        Toast.makeText(requireActivity(), msg, Toast.LENGTH_SHORT).show()
+
+                    }
+
+                    override fun onImageSaved(file: File) {
+                        val msg = "Photo capture successfully: ${file.path}"
+                        Toast.makeText(requireActivity(),msg, Toast.LENGTH_SHORT).show()
+                        Log.d(TAG, "${file.absolutePath}")
+                    }
+                })
+
+        }
+
+
+        CameraX.bindToLifecycle(this, preview, imageCapture)
+
+
+    }
+
+    private fun updateTransform() {
+        val matrix = Matrix()
+        val centerX = texture.width / 2f
+        val centerY = texture.height / 2f
+
+        val rotationDegrees = when (texture.display.rotation) {
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 90
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+            else -> return
+        }
+        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
+        texture.setTransform(matrix)
+    }
+
+    companion object {
+        private const val TAG = "CameraXBasic"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS =
+            arrayOf(
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            requireActivity(), it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray
+    ) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                texture.post { startCamera() }
+            } else {
+                Toast.makeText(
+                    requireActivity(),
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+}
